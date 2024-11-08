dda762a114257ab8d8d619bbc575c71b71c4c389
Ayush Khosla <ayush.k@justbaat.com>
2024-11-09 02:29:19 +0530

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <>
+      <main className="max-w-xl mx-auto flex">
+        <div>
+          <h1>Generate a Video from a URL using AI</h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores quo facere quibusdam voluptatibus. Possimus ad, beatae modi totam dignissimos nisi, reprehenderit accusamus, illo quam tenetur error voluptatibus ut quidem sint?
+          </p>
+          <form>
+            <input type="url" placeholder="https://..." />
+            <button type="submit">Generate Video</button>
+          </form>
+        </div>
+        <div>right</div>
+      </main>
+    </>
+  );
+}
+
+export default App;
