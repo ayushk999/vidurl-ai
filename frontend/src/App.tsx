719c0ec356ebf4dccf53a01361d4df5091e86275
Ayush Khosla <ayush.k@justbaat.com>
2024-11-10 01:44:03 +0530

    landing page done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,34 @@
 function App() {
   return (
     <>
-      <main className="max-w-xl mx-auto flex">
-        <div>
-          <h1>Generate a Video from a URL using AI</h1>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores quo facere quibusdam voluptatibus. Possimus ad, beatae modi totam dignissimos nisi, reprehenderit accusamus, illo quam tenetur error voluptatibus ut quidem sint?
-          </p>
-          <form>
-            <input type="url" placeholder="https://..." />
-            <button type="submit">Generate Video</button>
+      <main className="max-w-2xl mx-auto gap-8 flex px-4">
+        <div className="py-8 flex flex-col justify-center">
+          <h1 className="text-4xl font-bold uppercase mb-4">
+            <span className="text-5xl">URL to Video</span>
+            <br />
+            <span className="bg-gradient-to-br from-emerald-300 from-30% to-sky-500 bg-clip-text text-transparent">
+              with power of AI
+            </span>
+          </h1>
+          <form className="grid gap-2 ">
+            <input
+              className="border-2 rounded-full bg-transparent text-white px-4 py-2 grow"
+              type="url"
+              placeholder="https://..."
+            />
+            <button
+              className="bg-emerald-500 text-white px-4 py-2 rounded-full uppercase"
+              type="submit"
+            >
+              Generate&nbsp;Video
+            </button>
           </form>
         </div>
-        <div>right</div>
+        <div className="py-4">
+          <div className="bg-gray-200 w-[240px] h-[380px] text-gray-500 rounded-2xl p-8">
+            video here
+          </div>
+        </div>
       </main>
     </>
   );
